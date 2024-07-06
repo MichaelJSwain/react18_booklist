7255f05dbc00f897a77a43e3944705f188b614b5
MichaelSwain <michaeljswain@gmail.com>
2024-07-06 06:10:34 +0200

    add initial styles for book and booklist components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+// if own asset, need to provide the path to the asset
+import './index.css';
 
 
 const BookList = () => {
     return (
-        <section>
+        <section className="booklist">
             <Book />
             <Book />
             <Book />
@@ -15,7 +17,7 @@ const BookList = () => {
 
 const Book = () => {
     return (
-        <article>
+        <article className="book">
             <Image/>
             <Title />
             <Author />
@@ -23,11 +25,11 @@ const Book = () => {
     )
 }
 
-const Image = () => <h2>placeholder image</h2>
+const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/913C+MR3S5L._AC_UL900_SR900,600_.jpg' alt='The Women: A Novel'/>
 const Title = () => {
-    return <h2>Book Title</h2>
+    return <h2>The Women: A Novel</h2>
 }
-const Author = () => <h4>Author</h4>
+const Author = () => <h4>Kristin Hannah</h4>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
