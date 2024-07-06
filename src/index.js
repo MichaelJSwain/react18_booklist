021eaba43699bdded34026574e15c0a207e4f2d6
MichaelSwain <michaeljswain@gmail.com>
2024-07-06 09:26:09 +0200

    add changes

--- src/index.js
+++ src/index.js
@@ -3,27 +3,41 @@ import ReactDOM from "react-dom/client";
 // if own asset, need to provide the path to the asset
 import './index.css';
 
-const title = "The Women: A Novel";
-const author = "Kristin Hannah";
-const imgUrl = "https://images-na.ssl-images-amazon.com/images/I/913C+MR3S5L._AC_UL900_SR900,600_.jpg"
+const firstBook = {
+    title: "The Women: A Novel",
+    author: "Kristin Hannah",
+    imgUrl: "https://images-na.ssl-images-amazon.com/images/I/913C+MR3S5L._AC_UL900_SR900,600_.jpg"
+}
+const secondBook = {
+    title: "Reckless (The Powerless Trilogy)",
+    author: "Lauren Roberts",
+    imgUrl: "https://images-na.ssl-images-amazon.com/images/I/81q6ecxcZUL._AC_UL900_SR900,600_.jpg"
+}
+
 
 const BookList = () => {
     return (
         <section className="booklist">
-            <Book title={title} author={author} imgUrl={imgUrl}/>
-            <Book title={title} author={author} imgUrl={imgUrl}/>
+            <Book title={firstBook.title} author={firstBook.author} imgUrl={firstBook.imgUrl}>
+                <p>lorem ipsum</p>
+                <button>Click me</button>
+            </Book>
+            <Book title={secondBook.title} author={secondBook.author} imgUrl={secondBook.imgUrl}/>
         </section>
     )
 }
 
 const Book = (props) => {
-    const {title, author, imgUrl} = props;
+    const {title, author, imgUrl, children} = props;
+
+    console.log(props);
 
     return (
         <article className="book">
             <img src={imgUrl} alt={title}/>
             <h2>{title}</h2>
             <h4>{author}</h4>
+            {children}
         </article>
     )
 }
